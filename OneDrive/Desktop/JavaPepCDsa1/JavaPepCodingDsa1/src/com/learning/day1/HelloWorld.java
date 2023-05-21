8deaf18cb53df5bac8fadebb3c42fc462a0bd4af
priya vardhan <vardhanpriya001@gmail.com>
2023-05-21 08:51:29 +0530

    addFun method call

---
+++ OneDrive/Desktop/JavaPepCDsa1/JavaPepCodingDsa1/src/com/learning/day1/HelloWorld.java
@@ -0,0 +1,19 @@
+package com.learning.day1;
+import java.util.Scanner;
+
+public class HelloWorld{
+
+    public static void main(String[] args) {
+        int c = addFun(4,7);
+        System.out.println(c);
+        //int a=5;
+        //int b=6;
+        //System.out.println(" a + b = " + (a+b) );
+    }
+
+    public static int addFun(int a,int b){
+        return a+b;
+    }
+
+
+}
